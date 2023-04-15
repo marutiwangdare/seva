215d1cd6682a8bdbdfe5cb00027e7bce12f388cd
marutiwangdare <marutiedu999@gmail.com>
2023-04-15 19:52:21 +0530

    use case dfd word doc

---
+++ storage/logs/log-2023-04-15.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2023-04-15 16:42:32 --> Severity: Warning --> Undefined variable $result_json D:\xampp\htdocs\seva\application\controllers\admin\Dashboard.php 89
+ERROR - 2023-04-15 16:44:51 --> Severity: Warning --> Undefined variable $result_json D:\xampp\htdocs\seva\application\controllers\admin\Dashboard.php 89
+ERROR - 2023-04-15 17:26:27 --> Severity: Warning --> Undefined variable $result_json D:\xampp\htdocs\seva\application\controllers\admin\Dashboard.php 89
